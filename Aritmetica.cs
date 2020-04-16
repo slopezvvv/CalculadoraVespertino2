bad4b58a898f1c6190edcb6413e996b8ebc670c8
yo <yo@DESKTOP-RVOAHLO>
2020-04-15 22:07:20 -0400

    initial commit.

---
+++ Aritmetica.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraVespertino
+{
+    abstract class Aritmetica
+    {
+        public static double Suma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Resta(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Division(double dividendo, double divisor)
+        {
+            if (dividendo == 0) return 0;
+            if(divisor == 0)
+            {
+                Console.WriteLine("No se puede realizar la division por cero");
+                return 0;
+            }
+            if (dividendo == 0 && divisor == 0)
+            {
+                Console.WriteLine("Resultado indefinido");
+                return 0;
+            }
+            return dividendo / divisor;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace CalculadoraVespertino
+{
+    enum Operaciones
+    {
+        SUMA, RESTA, MULTIPLICACION, DIVISION
+    }
+
+    class Program
+    {
+        static void PedirFactores(Operaciones op)
+        {
+            Console.WriteLine("Ingrese el primer factor: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo factor: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double resultado = 0;
+            switch (op) {
+                case Operaciones.SUMA:
+                    resultado = Aritmetica.Suma(a, b);
+                    break;
+                case Operaciones.RESTA:
+                    resultado = Aritmetica.Resta(a, b);
+                    break;
+                case Operaciones.MULTIPLICACION:
+                    resultado = Aritmetica.Multiplicar(a, b);
+                    break;
+                case Operaciones.DIVISION:
+                    resultado = Aritmetica.Division(a, b);
+                    break;
+            }
+
+            Console.WriteLine("El resultado es: "+resultado);
+        }
+
+        static void ImprimirMenu()
+        {
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicacion");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("0. Salir");
+        }
+
+        static int ValidarInput()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                return int.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la calculadora! ingrese su opcion:");
+            bool estado = true;
+            while (estado)
+            {
+                ImprimirMenu();
+                int opcion = ValidarInput();
+
+                double a = 0, b = 0;
+                switch (opcion)
+                {
+                    case 1:
+                        PedirFactores(Operaciones.SUMA);
+                        break;
+                    case 2:
+                        PedirFactores(Operaciones.RESTA);
+                        break;
+                    case 3:
+                        PedirFactores(Operaciones.MULTIPLICACION);
+                        break;
+                    case 4:
+                        PedirFactores(Operaciones.DIVISION);
+                        break;
+                    case 0:
+                        estado = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+        
+    }
+}
